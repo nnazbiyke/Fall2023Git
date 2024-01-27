05811e7653770ec2efb74aac3850ea08d896b81b
Nazbiike <nnazbiyke05@gmail.com>
2024-01-28 01:54:53 +0600

    scrum-2345678/Feature--> Edited Main class

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    // git init - create local repository
-    // git config user.name "" -- create who use git
-    // git config user.name --> show who use git
-    // git config user.email "" -->
-    // git config user.email
-    // git checkout -b Nazbiike --> create new branch and switch auto
+
 }
